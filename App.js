30a7775fc8d39f7137a75f8007cdf91d9dd3c08a
Perkins <lrperkins@labs.neit.edu>
2020-01-09 13:49:37 -0500

    first setup for static express app

---
+++ App.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router();
+
+//Setup first route
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/views/index.html"));
+});
+
+//Setup About route
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname+"/views/about.html"));
+});
+
+//Setup contact route
+app.get('/contact', function(req, res){
+    res.sendFile(path.join(__dirname+"/views/contact.html"));
+});
+app.use(express.static(__dirname+"/views"));
+app.use('/', router);
+
+app.listen(5000, function(){
+
+    console.log("Server running, port 5000");
+
+});
